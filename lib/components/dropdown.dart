76c4e7f5608863bf20afe2c8a0ad4bfeb323385c
Viz <vishakhkanthi@gmail.com>
2024-03-21 12:19:21 +0530

    dropdowns working as intended

--- lib/components/dropdown.dart
+++ lib/components/dropdown.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart'; // Firebase Firestore dependency
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:gofurthr/components/globals.dart';
 
 class SimpleDropDown extends StatefulWidget {
   const SimpleDropDown({Key? key}) : super(key: key);
@@ -16,33 +17,55 @@ class _SimpleDropDownState extends State<SimpleDropDown> {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
-      //first dropdown
-
       stream: FirebaseFirestore.instance.collection('vehicleData').snapshots(),
       builder: (context, snapshot) {
+        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+          return const CircularProgressIndicator(); // or any other loading indicator
+        }
+
         final vehicleIds = snapshot.data!.docs.map((doc) => doc.id).toList();
 
-        return Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 10),
-          child: Column(
-            children: [
-              DropdownButtonFormField<String>(
-                value: _selectedVehId,
-                hint: Text(_selectedVehId ?? 'Select Vehicle'),
-                items: vehicleIds
-                    .map((id) => DropdownMenuItem(
-                          value: id,
-                          child: Text(id),
-                        ))
-                    .toList(),
-                onChanged: (value) => setState(() {
-                  _selectedVehId = value;
-                  _selectedBrandId = null;
-                  _selectedModelId = null;
-                }),
+        return Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: secondary,
+                borderRadius: BorderRadius.circular(18),
               ),
-
-              // Conditionally render the second dropdown
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 10,
+                  vertical: 10,
+                ),
+                child: DropdownButtonFormField<String>(
+                  value: _selectedVehId,
+                  iconEnabledColor: Colors.white,
+                  dropdownColor: Colors.black,
+                  menuMaxHeight: 400,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'Coolvetica',
+                    fontSize: 18,
+                  ),
+                  hint: Text(_selectedVehId ?? 'Select Vehicle',
+                      style: const TextStyle(color: Colors.white)),
+                  items: vehicleIds
+                      .map((id) => DropdownMenuItem(
+                            value: id,
+                            child: Text(id),
+                          ))
+                      .toList(),
+                  onChanged: (value) => setState(() {
+                    _selectedVehId = value;
+                    _selectedBrandId = null;
+                    _selectedModelId = null;
+                  }),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            if (_selectedVehId !=
+                null) // Display the second dropdown if a vehicle is selected
               StreamBuilder<QuerySnapshot>(
                 stream: FirebaseFirestore.instance
                     .collection('vehicleData')
@@ -50,28 +73,53 @@ class _SimpleDropDownState extends State<SimpleDropDown> {
                     .collection('Brands')
                     .snapshots(),
                 builder: (context, snapshot) {
+                  if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                    return const CircularProgressIndicator(); // or any other loading indicator
+                  }
+
                   final brandIds =
                       snapshot.data!.docs.map((doc) => doc.id).toList();
 
-                  return DropdownButtonFormField<String>(
-                    value: _selectedBrandId,
-                    hint: Text(_selectedBrandId ?? 'Select Brand'),
-                    items: brandIds
-                        .map((id) => DropdownMenuItem(
-                              value: id,
-                              child: Text(id),
-                            ))
-                        .toList(),
-                    onChanged: (value) => setState(() {
-                      _selectedBrandId = value;
-                      _selectedModelId = null;
-                    }),
+                  return Container(
+                    decoration: BoxDecoration(
+                      color: secondary,
+                      borderRadius: BorderRadius.circular(18),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 10,
+                      ),
+                      child: DropdownButtonFormField<String>(
+                        iconEnabledColor: Colors.white,
+                        dropdownColor: Colors.black,
+                        menuMaxHeight: 400,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'Coolvetica',
+                          fontSize: 18,
+                        ),
+                        value: _selectedBrandId,
+                        hint: Text(_selectedBrandId ?? 'Select Brand',
+                            style: const TextStyle(color: Colors.white)),
+                        items: brandIds
+                            .map((id) => DropdownMenuItem(
+                                  value: id,
+                                  child: Text(id),
+                                ))
+                            .toList(),
+                        onChanged: (value) => setState(() {
+                          _selectedBrandId = value;
+                          _selectedModelId = null;
+                        }),
+                      ),
+                    ),
                   );
                 },
               ),
-
-              //third dropdown
-
+            const SizedBox(height: 20),
+            if (_selectedBrandId !=
+                null) // Display the third dropdown if a brand is selected
               StreamBuilder<QuerySnapshot>(
                 stream: FirebaseFirestore.instance
                     .collection('vehicleData')
@@ -81,26 +129,50 @@ class _SimpleDropDownState extends State<SimpleDropDown> {
                     .collection('Models')
                     .snapshots(),
                 builder: (context, snapshot) {
-                  final brandIds =
+                  if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                    return const CircularProgressIndicator(); // or any other loading indicator
+                  }
+
+                  final modelIds =
                       snapshot.data!.docs.map((doc) => doc.id).toList();
 
-                  return DropdownButtonFormField<String>(
-                    value: _selectedModelId,
-                    hint: Text(_selectedModelId ?? 'Select Model'),
-                    items: brandIds
-                        .map((id) => DropdownMenuItem(
-                              value: id,
-                              child: Text(id),
-                            ))
-                        .toList(),
-                    onChanged: (value) => setState(() {
-                      _selectedModelId = value;
-                    }),
+                  return Container(
+                    decoration: BoxDecoration(
+                      color: secondary,
+                      borderRadius: BorderRadius.circular(18),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 10,
+                      ),
+                      child: DropdownButtonFormField<String>(
+                        iconEnabledColor: Colors.white,
+                        dropdownColor: Colors.black,
+                        menuMaxHeight: 400,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'Coolvetica',
+                          fontSize: 18,
+                        ),
+                        value: _selectedModelId,
+                        hint: Text(_selectedModelId ?? 'Select Model',
+                            style: const TextStyle(color: Colors.white)),
+                        items: modelIds
+                            .map((id) => DropdownMenuItem(
+                                  value: id,
+                                  child: Text(id),
+                                ))
+                            .toList(),
+                        onChanged: (value) => setState(() {
+                          _selectedModelId = value;
+                        }),
+                      ),
+                    ),
                   );
                 },
               ),
-            ],
-          ),
+          ],
         );
       },
     );
--- lib/pages/functionalities/create_veh.dart
+++ lib/pages/functionalities/create_veh.dart
@@ -12,31 +12,28 @@ class AddVeh extends StatelessWidget {
         preferredSize: const Size(double.maxFinite, 50),
         child: custAB('HOME PAGE', context),
       ),
-      body: SafeArea(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(25),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                //title
-                const Text(
-                  "Add a new vehicle!",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 40,
+      body: const SafeArea(
+        child: SingleChildScrollView(
+          child: Center(
+            child: Padding(
+              padding: EdgeInsets.all(25),
+              child: Column(
+                children: [
+                  //title
+                  Text(
+                    "Add a new vehicle!",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40,
+                    ),
                   ),
-                ),
 
-                //dropdown
-                Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.white),
-                  child: const SimpleDropDown(),
-                ),
-                //back button needs to be changed
-              ],
+                  SizedBox(height: 50),
+                  //dropdown
+                  SimpleDropDown(),
+                  //back button needs to be changed
+                ],
+              ),
             ),
           ),
         ),
